f804c50c9abb5fb006c5e0c91baf480f7aa63ef6
Anand Pap <anandpap@live.com>
2021-09-16 12:46:09 +0200

    Declaring string variables

---
+++ ex10/src/index.js
@@ -0,0 +1,13 @@
+function main(){
+    // only change code below this line
+    var myFirstName = "Anand", myLastName = "Pap", myHomeTown = "Sarajevo";
+    // only change code above this line
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+module.exports = main;
