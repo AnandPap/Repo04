59321997353b68a9676b301452570652bd71c5d5
Anand Pap <anandpap@live.com>
2021-09-16 11:07:48 +0200

    Dividing two numbers

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,6 +1,6 @@
 function main(numOne, numTwo) {
 
-    var quotient, numOne, numTwo;
+    var quotient;
     if(numTwo === 0) {
         quotient = 'Divide by 0! Cannot divide by zero!';
     } else {
