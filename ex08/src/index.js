59fdce246841ae76721a132f884915b543f557ff
Anand Pap <anandpap@live.com>
2021-09-17 03:31:58 +0200

    Compound assignment

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -1,6 +1,8 @@
 function main(){
     // only change code below this line
-    var myX = 12, myY = 19, myZ = 24;
+    var myX = 12;
+    var myY = 19;
+    var myZ = 24;
     myX += 3;
     myY += 17;
     myZ -= 20;
