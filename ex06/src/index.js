760d722682741ed59ecbf3e896f356e0b8b40170
Anand Pap <anandpap@live.com>
2021-09-17 03:23:04 +0200

    Dividing one decimal by another

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,6 +1,6 @@
 function main(numOne, numTwo){
     // only change code below this line
-    var myQuotient, numOne, numTwo;
+    var myQuotient;
     myQuotient = numOne / numTwo;
     // only change code above this line
     return myQuotient;
